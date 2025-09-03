236d403f19229ec4a3f76ddfaa7af87db7b04623
gh-leangains <hello@leangains.digital>
2025-09-03 08:06:06 +0100

    Add BankTransaction model class - placeholder for bank transaction data

---
+++ src/main/java/com/eretailgoals/model/BankTransaction.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.eretailgoals.model;
+
+/**
+ *
+ * @author Maqsud
+ */
+public class BankTransaction {
+
+
+}
